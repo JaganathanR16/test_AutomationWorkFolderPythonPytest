2b566b988afa2108f042c9814880d7572932bdfa
JaganathanR <>
2025-09-24 13:27:47 +0530

    first commit test

--- 2_test_pytest2/test_TestMethod3.py
+++ 2_test_pytest2/test_TestMethod3.py
@@ -3,8 +3,8 @@
 
 #pip install pytest
 #1) python -m pytest
-#2) python -m pytest -v
-#3) python -m pytest -v -s
+#2) python -m pytest -v       #Vargose will give more information [ie., Case directory and etc]
+#3) python -m pytest -v -s    #Console print "Hellow"
 ###########################
 #pip install pytest-html
 ##pytest --html=report.html
@@ -13,10 +13,13 @@
 ###########################Copy report.html path and past in browser to display report
 #file:///D:/test_AutomationWorkFolderPythonPytest/test_pytest/report.html?sort=result
 ################################
+#3) python -m pytest -v -s                      #Console print "Hellow"
+#4) python -m pytest -k CreditCard -v -s        #Run only method name contains "CreditCard" [ie., selected module test case]
+
 def test_firstProgramme():
     print("Hellow1")
 
 
-def test_secondProgramme():
+def test_secondProgrammeCreditCard():
     print("Hellow2")
     
