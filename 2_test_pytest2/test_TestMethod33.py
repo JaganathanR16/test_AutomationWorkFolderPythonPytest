c059a14517aa5c101c52bc842b1354e302220222
JaganathanR <>
2025-09-24 13:14:05 +0530

    first commit 43

---
+++ 2_test_pytest2/test_TestMethod33.py
@@ -0,0 +1,22 @@
+#pytest method requied and starting with test_
+#Pytest Rahul Shetty video
+
+#pip install pytest
+#1) python -m pytest
+#2) python -m pytest -v   #Vargose will give more information [ie., Case directory and etc]
+#3) python -m pytest -v -s
+
+def test_threeProgramme():
+    #print("Hellow3")
+    msg = "Hello 3"
+    assert msg == "Hi", "Test Failed"
+
+
+def test_fourProgrammeCreditCard():
+    a = 4
+    b = 6
+    assert a + 2 == 6, "Add do not match"
+
+
+#python -m pytest -v -s
+#python -m pytest -v -s --html=report.html
