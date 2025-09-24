fe3478a5a0e70de4bfb25fe81c3ecdd66f4fca27
JaganathanR <>
2025-09-24 13:21:30 +0530

    Update: remove test_TestMethod33.py, add test_TestMethod4.py

--- 2_test_pytest2/test_TestMethod33.py
+++ 2_test_pytest2/test_TestMethod33.py
@@ -3,20 +3,20 @@
 
 #pip install pytest
 #1) python -m pytest
-#2) python -m pytest -v   #Vargose will give more information [ie., Case directory and etc]
+#2) python -m pytest -v
 #3) python -m pytest -v -s
-
-def test_threeProgramme():
-    #print("Hellow3")
-    msg = "Hello 3"
-    assert msg == "Hi", "Test Failed"
-
-
-def test_fourProgrammeCreditCard():
-    a = 4
-    b = 6
-    assert a + 2 == 6, "Add do not match"
-
-
-#python -m pytest -v -s
+###########################
+#pip install pytest-html
+##pytest --html=report.html
+###########################HTML Report will create
 #python -m pytest -v -s --html=report.html
+###########################Copy report.html path and past in browser to display report
+#file:///D:/test_AutomationWorkFolderPythonPytest/test_pytest/report.html?sort=result
+################################
+def test_firstProgramme():
+    print("Hellow1")
+
+
+def test_secondProgramme():
+    print("Hellow2")
+    
